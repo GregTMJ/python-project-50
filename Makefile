dc3b6eff65548705d6e036956e22ab0768461b4f
Салиба Грегори Ибрагимович <djangoadmin@asupg.workgroup>
2022-10-04 11:29:58 +0300

    formatOptions

---
+++ Makefile
@@ -0,0 +1,17 @@
+install:
+	poetry install
+
+build:
+	poetry build
+
+publish:
+	poetry publish --dry-run
+
+package-install:
+	python -m pip install --force-reinstall dist/*.whl
+
+gendiff:
+	poetry run gendiff
+
+lint:
+	poetry run flake8 hexlet_code
---
+++ hexlet_code/scripts/gendiff.py
@@ -0,0 +1,22 @@
+import argparse
+
+
+def main() -> None:
+    """
+    function to get input args to start the game
+    """
+    parser = argparse.ArgumentParser(description='Compares two configuration '
+                                                 'files and shows a'
+                                                 ' difference')
+    parser.add_argument("first_file", type=str)
+    parser.add_argument("second_file", type=str)
+    parser.add_argument(
+            '-f',
+            '--format',
+            help='set format of output')
+    args = parser.parse_args()
+    print(args)
+
+
+if __name__ == '__main__':
+    main()
