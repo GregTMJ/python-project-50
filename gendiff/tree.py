de31f51e5ecf104b654b76b2f8b75558c577ad87
Салиба Грегори Ибрагимович <salibagi@nts.local>
2022-10-05 09:18:21 +0300

    treemakertestfix

--- gendiff/tree.py
+++ gendiff/tree.py
@@ -2,6 +2,7 @@
 Script that transforms the 2 dicts into a tree
 """
 
+
 def build(old_node: dict, new_node: dict) -> list:
     """
     helps add items to the tree in the needed order
@@ -14,33 +15,33 @@ def build(old_node: dict, new_node: dict) -> list:
                 "key": key,
                 "type": "deleted",
                 "value": old_node[key]
-                })
+            })
         elif key not in old_node:
             result.append({
                 "key": key,
                 "type": "added",
                 "value": new_node[key]
-                })
+            })
         elif isinstance(old_node[key], dict) \
                 and isinstance(new_node[key], dict):
-             result.append({
-                 "key": key,
-                 "type": "nested",
-                 "children": build(old_node[key], new_node[key])
-                 })
+            result.append({
+                "key": key,
+                "type": "nested",
+                "children": build(old_node[key], new_node[key])
+            })
         elif old_node[key] != new_node[key]:
             result.append({
                 "key": key,
                 "type": "changed",
                 "value1": old_node[key],
                 "value2": new_node[key]
-                })
+            })
         else:
             result.append({
                 "key": key,
                 "type": "unchanged",
                 "value": old_node[key]
-                })
+            })
     return result
 
 
@@ -49,6 +50,6 @@ def make_tree(node1: dict, node2: dict) -> dict:
     Generates a tree
     """
     return {
-            "type": "root",
-            "children": build(node1, node2)
-            }
+        "type": "root",
+        "children": build(node1, node2)
+    }
--- tests/tree_test.py
+++ tests/tree_test.py
@@ -1,5 +1,6 @@
 from gendiff.tree import make_tree
 
+
 def test_tree_maker():
     """
     Tests the function make_tree
@@ -24,46 +25,21 @@ def test_tree_maker():
         }
     }
     func_result = make_tree(sample_1, sample_2)
-    assert func_result == {
-        "type": "root",
-        "children": [
-            {
-                "key": "name",
-                "type": "unchanged",
-                "value": "Greg"
-            },
-            {
-                "key": "surname",
-                "type": "changed",
-                "value1": "Saliba",
-                "value2": "Alexandrov"
-            },
-            {
-                "key": "date_of_birth",
-                "type": "deleted",
-                "value": "14-06-1996"
-            },
-            {
-                "key": "person_data",
-                "type": "nested",
-                "children": [
-                    {
-                        "key": "age",
-                        "type": "unchanged",
-                        "value": 26
-                    },
-                    {
-                        "key": "height",
-                        "type": "changed",
-                        "value1": 183,
-                        "value2": 178
-                    },
-                    {
-                        "key": "weight",
-                        "type": "added",
-                        "value": 85
-                    }
-                ]
-            }
-        ]
-    }
+    print(func_result)
+    assert func_result == {'children': [{'key': 'date_of_birth',
+                                         'type': 'deleted',
+                                         'value': '14-06-1996'},
+                                        {'key': 'name', 'type': 'unchanged', 'value': 'Greg'},
+                                        {'children': [{'key': 'age', 'type': 'unchanged', 'value': 26},
+                                                      {'key': 'height',
+                                                       'type': 'changed',
+                                                       'value1': 183,
+                                                       'value2': 178},
+                                                      {'key': 'weight', 'type': 'added', 'value': 85}],
+                                         'key': 'person_data',
+                                         'type': 'nested'},
+                                        {'key': 'surname',
+                                         'type': 'changed',
+                                         'value1': 'Saliba',
+                                         'value2': 'Alexandrov'}],
+                           'type': 'root'}
