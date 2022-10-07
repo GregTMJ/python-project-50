13b769c8bed86c1f783f9214e33465305f586a92
Салиба Грегори Ибрагимович <salibagi@nts.local>
2022-10-07 09:02:08 +0300

    linter_fix_3

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -12,12 +12,11 @@ def main() -> None:
                                                  ' difference')
     parser.add_argument("first_file", type=str)
     parser.add_argument("second_file", type=str)
-    parser.add_argument(
-            '-f',
-            '--format',
-            dest='format',
-            default='stylish',
-            help='set format of output')
+    parser.add_argument('-f',
+                        '--format',
+                        dest='format',
+                        default='stylish',
+                        help='set format of output')
     args = parser.parse_args()
     diff = generate_diff(args.first_file, args.second_file, args.format)
     print(diff)
